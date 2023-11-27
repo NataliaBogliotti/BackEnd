0355975085b60b3636bdf082524a1f1e1b82ad30
NataliaBogliotti <128662130+NataliaBogliotti@users.noreply.github.com>
2023-11-26 21:37:04 -0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ server.use(middlewares);
 // Add this before server.use(router)
 server.use(
   // Add custom route here if needed
-  jsonServer.rewritter({
+  jsonServer.rewriter({
     "/": "/$1",
   })
 );
